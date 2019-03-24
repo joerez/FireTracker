d2023faa53d105b29139d1e6cc86b16fced20a74
Joerez <rezenbiz@gmail.com>
2019-03-24 01:21:52 -0700

    alpha 0.1

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -8,9 +8,9 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <h1>FireTracker</h1>
+        <h1>FireTracker.io</h1>
         <h3>Gain <span class="larger-letter">F</span>inancial <span class="larger-letter">I</span>ndependence and <span class="larger-letter">R</span>etire <span class="larger-letter">E</span>arly.</h3>
-        <h3>Track your progress to <span class="red larger-letter">FIRE</span>!</h3>
+        <h3>Track your progress to <span class="red larger-letter">FIRE!</span></h3>
         {this.props.auth ? <Link className="call-to-action" to="/dashboard">Go to dashboard</Link> : <a className="call-to-action" href="/auth/google">Try for free</a>}
 
         <div className="bottom">
