bcc59517eb9133157592240714467489eb33049c
Joerez <rezenbiz@gmail.com>
2019-03-26 09:41:09 -0700

    ui changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
   render() {
     return (
       <div className="nav">
-        <Link to={this.props.auth ? '/dashboard' : '/'} className="nav-brand nav-item">FireTracker</Link>
+        <Link to={this.props.auth ? '/dashboard' : '/'} className="nav-brand nav-item">{this.props.auth ? '' : 'FireTracker'}</Link>
         {this.props.auth ? <a href="/api/logout" className="nav-login nav-item">Logout</a> : <a href="/auth/google" className="nav-login nav-item">Login</a>}
       </div>
     )
