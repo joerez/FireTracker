cf1ca0e1e479d14725cd904b9993ff90e55585fc
Joerez <rezenbiz@gmail.com>
2019-04-09 21:59:54 -0700

    remove

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-//prod.js - production keys
-module.exports = {
-  googleClientID: '114725069889-bdo35q7pogbtfqo26p9mc42842urcg8j.apps.googleusercontent.com',
-  googleClientSecret: 'kh7lxMMkK6BmjWhIsFxP3NJl',
-  cookieKey: 'cookie',
-}
